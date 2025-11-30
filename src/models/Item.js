46eb9f2f800ab4d581f6b5e673afce2c48a3bf85
cuongalienat <cuongalienat@gmail.com>
2025-11-30 23:41:58 +0700

    adjust model Item

--- src/models/Item.js
+++ src/models/Item.js
@@ -3,13 +3,14 @@ import mongoose from "mongoose";
 //Menu 
 const itemSchema = new mongoose.Schema({
     name: { type: String, required: [true, "Tên món ăn là bắt buộc"], trim: true },
-    description: { type: String, required: [true, "Mô tả món ăn là bắt buộc"],},
+    category: { type: String, required: true },
+    description: { type: String, required: [true, "Mô tả món ăn là bắt buộc"], },
     price: { type: Number, required: [true, "Giá tiền là bắt buộc"], min: [0, "Giá tiền không thể âm"] },
-    imageUrl: { type: String, required: [true, "URL hình ảnh là bắt buộc"],},
+    imageUrl: { type: String, required: [true, "URL hình ảnh là bắt buộc"], },
     // Quan trọng: Món ăn này thuộc về nhà hàng nào?
     shopId: { type: mongoose.Schema.Types.ObjectId, ref: 'shop', required: true }
 }, {
-    timestamps: true 
+    timestamps: true
 });
 
 itemSchema.index({ shopId: 1, categoryId: 1 });
