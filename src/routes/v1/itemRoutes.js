0383f46f64c0ee7e34dc4453d142e08dc16d76b6
ntd1928 <ndat526686@gmail.com>
2025-12-02 02:16:00 +0000

    add findbyname and findbyaddress

--- src/routes/v1/itemRoutes.js
+++ src/routes/v1/itemRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getAllItems, getItemById, createItem, updateItem, deleteItem } from '../../controllers/itemController.js';
+import { getAllItems, getItemById, createItem, updateItem, deleteItem, getItemsByName,getItemsByAddress } from '../../controllers/itemController.js';
 import { authMiddleware, isRestaurant } from "../../middlewares/authMiddlewares.js";
 
 
 const router = express.Router();
-
+router.get('/search/name', getItemsByName);       // GET /api/items/search/name?keyword=...
+router.get('/search/address', getItemsByAddress); // GET /api/items/search/address?keyword=...
 router.get('/', getAllItems); // Lấy tất cả món ăn (có thể lọc theo nhà hàng)
 router.get('/:id', getItemById);// Lấy chi tiết một món ăn
 router.post('/', authMiddleware, isRestaurant, createItem);
